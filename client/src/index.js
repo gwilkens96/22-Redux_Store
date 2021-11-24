c622ef39542ad0abe926ea4257431d5632b11e26
GriffinWilkens96 <gwilkens1234@gmail.com>
2021-11-23 19:23:20 -0500

    replaced extra reducers fiel

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers";
+import rootReducer from "./utils/reducers";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
