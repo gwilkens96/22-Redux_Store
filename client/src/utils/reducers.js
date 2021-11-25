7a615cd8ca90b7db0582d9a70046cc3fb8266a14
GriffinWilkens96 <gwilkens1234@gmail.com>
2021-11-24 22:32:55 -0500

    reduced reducers haha

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -12,15 +12,14 @@ import {
 
 const initialState = {
   products: [],
-  cart: [],
-  cartOpen: false,
   categories: [],
   currentCategory: "",
+  cart: [],
+  cartOpen: false,
 };
 
-export default function rootReducer(state = initialState, action) {
+export const reducers = (state = initialState, action) => {
   switch (action.type) {
-    // Returns a copy of state with an update products array. We use the action.products property and spread it's contents into the new array.
     case UPDATE_PRODUCTS:
       return {
         ...state,
@@ -33,13 +32,13 @@ export default function rootReducer(state = initialState, action) {
         cartOpen: true,
         cart: [...state.cart, action.product],
       };
+
     case ADD_MULTIPLE_TO_CART:
       return {
         ...state,
         cart: [...state.cart, ...action.products],
       };
-    // Returns a copy of state, sets the cartOpen to true and maps through the items in the cart.
-    // If the item's `id` matches the `id` that was provided in the action.payload, we update the purchase quantity.
+
     case UPDATE_CART_QUANTITY:
       return {
         ...state,
@@ -52,15 +51,11 @@ export default function rootReducer(state = initialState, action) {
         }),
       };
 
-    // First we iterate through each item in the cart and check to see if the `product._id` matches the `action._id`
-    // If so, we remove it from our cart and set the updated state to a variable called `newState`
     case REMOVE_FROM_CART:
       let newState = state.cart.filter((product) => {
         return product._id !== action._id;
       });
 
-      // Then we return a copy of state and check to see if the cart is empty.
-      // If not, we set the cartOpen status to  `true`. Then we return an updated cart array set to the value of `newState`.
       return {
         ...state,
         cartOpen: newState.length > 0,
@@ -92,9 +87,9 @@ export default function rootReducer(state = initialState, action) {
         currentCategory: action.currentCategory,
       };
 
-    // Return the state as is in the event that the `action.type` passed to our reducer was not accounted for by the developers
-    // This saves us from a crash.
     default:
       return state;
   }
-}
+};
+
+export default reducers;
